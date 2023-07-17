8b737448fd11e46256829b90277c20f56342af03
Benjamin Vogel <ben10vogel@gmail.com>
2023-07-17 11:23:17 -0500

    Example with exceptions on embedded

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,64 @@
+#![deny(unsafe_code)]
 #![no_std]
 #![no_main]
 
 // pick a panicking behavior
+// def profile: easier to debug panics; can put a breakpoint on `rust_begin_unwind`
+// #[cfg(debug_assertions)]
 use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-                     // use panic_abort as _; // requires nightly
-                     // use panic_itm as _; // logs messages over ITM; requires ITM support
-                     // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+
+// release profile: minimize the binary size of the application
+// #[cfg(not(debug_assertions))]
+// use panic_abort as _;
+// use panic_abort as _; // requires nightly
+// use panic_itm as _; // logs messages over ITM; requires ITM support
+// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+
+use core::fmt::Write;
 
 use cortex_m::asm;
-use cortex_m::peripheral::{syst, Peripherals};
-use cortex_m_rt::entry;
+use cortex_m::peripheral::syst::SystClkSource;
+use cortex_m_rt::{entry, exception};
+use cortex_m_semihosting::{
+    debug,
+    hio::{self, HStdout},
+};
 
 #[entry]
 fn main() -> ! {
-    let peripherals = Peripherals::take().unwrap();
-    let mut systick = peripherals.SYST;
-    systick.set_clock_source(syst::SystClkSource::Core);
-    systick.set_reload(1_000);
-    systick.clear_current();
-    systick.enable_counter();
+    let p = cortex_m::Peripherals::take().unwrap();
+    let mut syst = p.SYST;
 
-    while !systick.has_wrapped() {
-        // loop
-    }
+    // configures the system timer to trigger a SysTick exception every second
+    syst.set_clock_source(SystClkSource::Core);
+    // Thi sis configured for the LM3S6965 which has a default CPU clock of 12 MHz
+    syst.set_reload(12_000_000);
+    syst.clear_current();
+    syst.enable_counter();
+    syst.enable_interrupt();
 
     loop {}
+}
+
+#[exception]
+fn SysTick() {
+    static mut COUNT: u32 = 0;
+    static mut STDOUT: Option<HStdout> = None;
+
+    *COUNT += 1;
+
+    // Lazy initialization
+    if STDOUT.is_none() {
+        *STDOUT = hio::hstdout().ok();
+    }
+
+    if let Some(hstdout) = STDOUT.as_mut() {
+        write!(hstdout, "{}", *COUNT).ok();
+    }
+
+    // IMPORTANT omit this `if` block if running on real hardware or your debugger will end in an inconsistent state
+    if *COUNT == 9 {
+        // This will terminate the QEMU process
+        debug::exit(debug::EXIT_SUCCESS);
+    }
 }
